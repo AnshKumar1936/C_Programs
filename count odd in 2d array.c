b18d127ba54130c6020fa9674d7559dce4bbbcd2
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-02-15 10:37:52 +0530

    Add files via upload

---
+++ count odd in 2d array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int odd(int arr[] ,int n);
+
+int main()
+{
+	int arr[]={1,2,3,4,5,6};
+	
+	printf("%d", odd(arr,6) );
+
+	return 0;
+}
+
+
+int odd(int arr[], int n){
+	int odd=0;
+	for(int i=0;i<n;i++){
+		if(arr[i] % 2 != 0){
+			odd++;
+		}
+		
+	}
+	return odd;
+}
